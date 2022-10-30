0113c89c24d998fd1457dd3696cc9127e130099c
Rejone E Rasul Hridoy <Rejone.Rasul@brainstation-23.com>
2022-10-30 22:36:00 +0600

    style modified and data entry

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log('hello hridoy');
+function setActive(section){
+    for(i=0; i<document.getElementsByClassName('nav-option').length; i++){
+        let option = document.getElementsByClassName('nav-option')[i];
+        option.classList.remove('active');
+       console.log(option);
+    }
+    let sectionName = "nav-" + section;
+    document.getElementById(sectionName).classList.add('active');
+}
