5323e36f0634e16f80a8def129fae0b3b5140f9e
Rejone E Rasul Hridoy <Rejone.Rasul@brainstation-23.com>
2022-11-03 15:35:58 +0600

    code refactor

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ function setActive(section){
     for(i=0; i<document.getElementsByClassName('nav-option').length; i++){
         let option = document.getElementsByClassName('nav-option')[i];
         option.classList.remove('active');
-       console.log(option);
     }
     let sectionName = "nav-" + section;
     document.getElementById(sectionName).classList.add('active');
