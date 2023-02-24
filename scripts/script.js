a6691ac63add5e5f1df62b00b39552526396b79b
Rejone E Rasul Hridoy <Rejone.Rasul@brainstation-23.com>
2023-02-25 01:55:57 +0600

    updated resume to v5.0

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function setActive(section){
+    for(i=0; i<document.getElementsByClassName('nav-option').length; i++){
+        let option = document.getElementsByClassName('nav-option')[i];
+        option.classList.remove('active');
+    }
+    let sectionName = "nav-" + section;
+    document.getElementById(sectionName).classList.add('active');
+}
